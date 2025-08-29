0defe0722f926ffb42569328bc5fd2a0ffe64f21
sasirekha3 <ksasirekha@gmail.com>
2025-08-28 17:31:57 -0700

    feat: app hang instrumentation

--- Sources/AwsOpenTelemetryCore/MetricKit/AwsMetricKitConstants.swift
+++ Sources/AwsOpenTelemetryCore/MetricKit/AwsMetricKitConstants.swift
@@ -1,17 +1,15 @@
-
 /// Constants for MetricKit instrumentation.
 ///
 /// Provides standardized attribute names and event types following MetricKit naming convention.
 ///
 public class AwsMetricKitConstants {
-  
+
   // MARK: - Hang constants
-  
+
   /// Attribute name for hang duration
   public static let hangDuration = "hang.hang_duration"
-  
+
   /// Attribute name for hang `callStackTree`
   public static let hangCallStackTree = "hang.call_stack_tree"
-  
-  
+
 }
