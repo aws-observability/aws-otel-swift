eb742a7b5528c069cf7421b1c4555b966bcca758
sasirekha3 <ksasirekha@gmail.com>
2025-09-08 22:40:15 -0700

    feat: create contract test framework and tests (phase 9)

---
+++ Examples/SimpleAwsUiKitDemo/SimpleAwsUiKitDemo/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  SimpleAwsUiKitDemo
+//
+//  Created by Kambhampaty, Rekha on 9/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
