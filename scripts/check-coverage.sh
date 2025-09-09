c164b7698137dbefa5b36d821c56ba8e63dda65f
sasirekha3 <ksasirekha@gmail.com>
2025-09-08 18:50:03 -0700

    feat: create contract test framework and tests (phase 7)

--- scripts/check-coverage.sh
+++ scripts/check-coverage.sh
@@ -11,7 +11,7 @@ COVERAGE_FILE="coverage.txt"
 
 # Generate coverage report
 echo "Generating coverage report..."
-swift test --enable-code-coverage 
+swift test --enable-code-coverage --test-plan UnitTestPlan.xctestplan
 xcrun llvm-cov report .build/debug/aws-otel-swiftPackageTests.xctest/Contents/MacOS/aws-otel-swiftPackageTests -instr-profile .build/debug/codecov/default.profdata --format=text > "$COVERAGE_FILE"
 
 # Check repository coverage
