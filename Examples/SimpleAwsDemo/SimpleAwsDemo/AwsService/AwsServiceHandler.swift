953dfd85cc9b57e7eb93cfb42257556293648838
Billy Zhou <billyzh@amazon.com>
2025-09-16 17:53:16 -0700

    fix lint

--- Examples/SimpleAwsDemo/SimpleAwsDemo/AwsService/AwsServiceHandler.swift
+++ Examples/SimpleAwsDemo/SimpleAwsDemo/AwsService/AwsServiceHandler.swift
@@ -142,6 +142,6 @@ public class AwsServiceHandler {
       secret: creds.getSecret() ?? "",
       sessionToken: creds.getSessionToken()
     )
-      return try StaticAWSCredentialIdentityResolver(awsCredentials)
+    return try StaticAWSCredentialIdentityResolver(awsCredentials)
   }
 }
