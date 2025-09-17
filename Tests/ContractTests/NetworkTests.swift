115359c16ebc39f0b401dddd774455efc092c284
sasirekha3 <ksasirekha@gmail.com>
2025-09-17 12:31:48 -0700

    feat: create contract test framework and tests (phase 13)

--- Tests/ContractTests/NetworkTests.swift
+++ Tests/ContractTests/NetworkTests.swift
@@ -12,7 +12,7 @@ class NetworkTests: XCTestCase {
   private let HTTP_404_URL = "http://localhost:8181/404"
   private let HTTP_500_URL = "http://localhost:8181/500"
 
-  func testNetworkSpanIsCreated() {
+  func testNetworkSpansExistForGET() {
     let scopeSpans: [ScopeSpan] = data?.traces.flatMap { trace in
       trace.resourceSpans.flatMap { resourceSpan in
         resourceSpan.scopeSpans.filter { scopeSpan in
@@ -44,6 +44,34 @@ class NetworkTests: XCTestCase {
 
     XCTAssertTrue(hasGetMethod)
 
+    // Check server address exists
+    let serverAddr = spans.first?.attributes.first { attr in
+      attr.key == SERVER_ADDR_ATTR
+    }
+    XCTAssertNotNil(serverAddr?.value.stringValue)
+
+    // Check server port exists
+    let serverPort = spans.first?.attributes.first { attr in
+      attr.key == SERVER_PORT_ATTR
+    }
+    XCTAssertNotNil(serverPort?.value.intValue)
+  }
+
+  func testNetworkSpansExistFor200() {
+    let scopeSpans: [ScopeSpan] = data?.traces.flatMap { trace in
+      trace.resourceSpans.flatMap { resourceSpan in
+        resourceSpan.scopeSpans.filter { scopeSpan in
+          scopeSpan.scope.name == AwsInstrumentationScopes.URL_SESSION
+        }
+      }
+    } ?? []
+
+    let spans: [Span] = scopeSpans.flatMap { scopeSpan in
+      scopeSpan.spans.filter { span in
+        span.name == "HTTP GET"
+      }
+    }
+
     // Check for status code
     let hasStatusCode = spans.contains { span in
       let statusCodeAttribute = span.attributes.first { attr in
@@ -62,6 +90,92 @@ class NetworkTests: XCTestCase {
       return urlAttribute?.value.stringValue == HTTP_200_URL
     }
     XCTAssertTrue(has200URL)
+
+    // Check status code 200
+    let spans200 = spans.filter { span in
+      let urlAttribute = span.attributes.first { attr in
+        attr.key == URL_FULL
+      }
+      return urlAttribute?.value.stringValue == HTTP_200_URL
+    }
+    let status200 = spans200.first?.attributes.first { attr in
+      attr.key == STATUS_CODE_ATTR
+    }
+    XCTAssertEqual(status200?.value.intValue, "200")
+  }
+
+  // func testNetworkSpansExistFor404() {
+  //   let scopeSpans: [ScopeSpan] = data?.traces.flatMap { trace in
+  //     trace.resourceSpans.flatMap { resourceSpan in
+  //       resourceSpan.scopeSpans.filter { scopeSpan in
+  //         scopeSpan.scope.name == AwsInstrumentationScopes.URL_SESSION
+  //       }
+  //     }
+  //   } ?? []
+
+  //   let spans: [Span] = scopeSpans.flatMap { scopeSpan in
+  //     scopeSpan.spans.filter { span in
+  //       span.name == "HTTP GET"
+  //     }
+  //   }
+
+  //   // Check for status code
+  //   let hasStatusCode = spans.contains { span in
+  //     let statusCodeAttribute = span.attributes.first { attr in
+  //       attr.key == STATUS_CODE_ATTR
+  //     }
+  //     return statusCodeAttribute != nil
+  //   }
+
+  //   XCTAssertTrue(hasStatusCode)
+
+  //   // Check for HTTP 404 URL
+  //   let has404URL = spans.contains { span in
+  //     let urlAttribute = span.attributes.first { attr in
+  //       attr.key == URL_FULL
+  //     }
+  //     return urlAttribute?.value.stringValue == HTTP_404_URL
+  //   }
+  //   XCTAssertTrue(has404URL)
+
+  //   // Check status code 404
+  //   let spans404 = spans.filter { span in
+  //     let urlAttribute = span.attributes.first { attr in
+  //       attr.key == URL_FULL
+  //     }
+  //     return urlAttribute?.value.stringValue == HTTP_404_URL
+  //   }
+  //   let status404 = spans404.first?.attributes.first { attr in
+  //     attr.key == STATUS_CODE_ATTR
+  //   }
+  //   XCTAssertEqual(status404?.value.intValue, "404")
+
+  // }
+
+  func testNetworkSpansExistFor500() {
+    let scopeSpans: [ScopeSpan] = data?.traces.flatMap { trace in
+      trace.resourceSpans.flatMap { resourceSpan in
+        resourceSpan.scopeSpans.filter { scopeSpan in
+          scopeSpan.scope.name == AwsInstrumentationScopes.URL_SESSION
+        }
+      }
+    } ?? []
+
+    let spans: [Span] = scopeSpans.flatMap { scopeSpan in
+      scopeSpan.spans.filter { span in
+        span.name == "HTTP GET"
+      }
+    }
+
+    // Check for status code
+    let hasStatusCode = spans.contains { span in
+      let statusCodeAttribute = span.attributes.first { attr in
+        attr.key == STATUS_CODE_ATTR
+      }
+      return statusCodeAttribute != nil
+    }
+
+    XCTAssertTrue(hasStatusCode)
 
     // Check for HTTP 500 URL
     let has500URL = spans.contains { span in
@@ -83,17 +197,5 @@ class NetworkTests: XCTestCase {
       attr.key == STATUS_CODE_ATTR
     }
     XCTAssertEqual(status500?.value.intValue, "500")
-
-    // Check server address exists
-    let serverAddr = spans.first?.attributes.first { attr in
-      attr.key == SERVER_ADDR_ATTR
-    }
-    XCTAssertNotNil(serverAddr?.value.stringValue)
-
-    // Check server port exists
-    let serverPort = spans.first?.attributes.first { attr in
-      attr.key == SERVER_PORT_ATTR
-    }
-    XCTAssertNotNil(serverPort?.value.intValue)
   }
 }
